e87e3b18b28e539aabaa1b0bf1d3a52755b48beb
luxo-ai <luxo.ai@proton.me>
2023-09-21 21:19:07 -0400

    updates

--- src/useStatefulScreens.test.tsx
+++ src/useStatefulScreens.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks/dom';
 import useStatefulScreens from './useStatefulScreens';
 
 describe('useStatefulScreens Hook', () => {
